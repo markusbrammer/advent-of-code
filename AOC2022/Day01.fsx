2f5f6c82e5a2335b6580bdf192b6a8f57d621dab
Markus B. Jensen <s183816@student.dtu.dk>
2022-11-28 20:36:09 +0100

    Restructured.

---
+++ AOC2022/Day01.fsx
@@ -0,0 +1,5 @@
+#load "Utils.fsx"
+open Utils
+
+let test = readLines <| __SOURCE_DIRECTORY__ + "/inputs/InputDay01.txt"
+printfn "%A" test
--- Day01.fsx
+++
@@ -1,5 +0,0 @@
-#load "Utils.fsx"
-open Utils
-
-let test = readLines "inputs/InputDay01.txt"
-printfn "%A" test
