218a3dccd4b756e1663c0e99db145d4b33174516
Markus B. Jensen <s183816@student.dtu.dk>
2022-12-01 09:33:43 +0100

    Solved day 1

--- AOC2022/Day01.fsx
+++ AOC2022/Day01.fsx
@@ -1,5 +1,29 @@
 #load "Utils.fsx"
 open Utils
 
-let test = readInputLines <| "InputDay01.txt"
-printfn "%A" test
+let testInput = readInputLines "TestDay01.txt"
+let input = readInputLines "InputDay01.txt"
+
+(* === PART 1 === *)
+
+let sumOfInventories (calorieList: string list) =
+    // Add the sum of calories of each inventory to a list of sums. 
+    List.fold
+        (fun (sums, inventorySum) entry ->
+            match entry with
+            | "" -> (inventorySum :: sums, 0)
+            | s -> (sums, (int s) + inventorySum))
+        ([], 0)
+        calorieList
+    // Remember the last inventory. 
+    |> fun (sums, lastInventorySum) -> lastInventorySum :: sums
+
+printResult 1 (List.max <| sumOfInventories input)
+
+(* === PART 2 === *)
+
+sumOfInventories input
+|> List.sortDescending
+|> List.take 3
+|> List.sum
+|> printResult 2
